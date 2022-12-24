acdddcf30c7cefb5b6caa2229b557a8c4f8e283e
filipjovicccc <filipjovicccc@gmail.com>
2022-12-24 22:45:38 +0100

    first commit

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,23 @@
+export const TASKS = [
+   {
+    id: 1,
+    text: 'Doctors Appontment',
+    day: 'May 5th at 2:30pm',
+    reminder:true,
+
+   },
+   {
+    id: 2,
+    text: 'Meeting',
+    day: 'May 6th at 3:30pm',
+    reminder:true,
+   },
+   {
+    id: 3,
+    text: 'Shooping',
+    day: 'May 7th at 4:30pm',
+    reminder:true,
+   }
+
+
+]
